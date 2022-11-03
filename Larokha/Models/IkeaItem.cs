bfc8d76d7faa41d0123fdf8ee1b717181cc03f37
meti <mahdikhodarahimi0@gmail.com>
2022-11-03 12:19:55 +0330

    Add project files.

---
+++ Larokha/Models/IkeaItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Larokha.Models
+{
+    public class IkeaItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Star { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public Type Type { get; set; }
+        public string Category { get; set; }
+    }
+    public enum Type
+    {
+        New,
+        SecondHand
+    }
+}
